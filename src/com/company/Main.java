bbde91cc5889c3da52e1117f2bd5c7379afd4d10
Amarillia31 <mari31@inbox.ru>
2022-04-03 21:24:24 +0400

    init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Elena!");
+        System.out.println("2nd line");
+        System.out.println("3rd line");
+    }
+}
